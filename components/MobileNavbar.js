29e59b4cec5abd910edaf9ee244b51690ebf2404
jemmdevs <jemmdev@gmail.com>
2025-05-28 10:35:25 +0200

    uplobby app

--- components/MobileNavbar.js
+++ components/MobileNavbar.js
@@ -45,13 +45,13 @@ export default function MobileNavbar() {
         </Link>
 
         {/* Botón de explorar/buscar */}
-        <Link href="/search" className="flex flex-col items-center justify-center">
-          <div className={`p-2 rounded-full ${pathname === '/search' ? 'bg-gray-100 dark:bg-gray-800' : ''}`}>
-            <svg xmlns="http://www.w3.org/2000/svg" className={`h-6 w-6 ${pathname === '/search' ? 'text-[var(--mongodb-dark-green)]' : 'text-gray-600 dark:text-gray-400'}`} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <Link href="/projects" className="flex flex-col items-center justify-center">
+          <div className={`p-2 rounded-full ${pathname === '/projects' ? 'bg-gray-100 dark:bg-gray-800' : ''}`}>
+            <svg xmlns="http://www.w3.org/2000/svg" className={`h-6 w-6 ${pathname === '/projects' ? 'text-[var(--mongodb-dark-green)]' : 'text-gray-600 dark:text-gray-400'}`} fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
             </svg>
           </div>
-          <span className="text-xs mt-1 font-medium text-gray-600 dark:text-gray-400">Explorar</span>
+          <span className="text-xs mt-1 font-medium text-gray-600 dark:text-gray-400">Buscar</span>
         </Link>
 
         {/* Botón de perfil */}
